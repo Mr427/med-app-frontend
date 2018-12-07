097fec42cd2f58f74beaa89ea30970d80c8b1ad5
John Linux19 <johnmichael.tuttle@gmail.com>
2018-12-07 17:14:02 -0500

    Removed React Tutorial Code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDom from"react-dom";
-import Game from "./components/app";
+import App from "./components/app";
 import "./style/scss/main-style.scss"
 
 const container = document.createElement("div");
 container.setAttribute("id", "root");
 document.body.appendChild(container);
 
-ReactDom.render(<Game />, container);
+ReactDom.render(<App />, container);
